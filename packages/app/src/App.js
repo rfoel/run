0644de8e99bf7b765a45b95ea7ab54c2a21b2575
Rafael Franco <rafaelfr@outlook.com>
2020-02-27 10:20:38 -0300

    feat: add 1e3 ui

--- packages/app/src/App.js
+++ packages/app/src/App.js
@@ -1,7 +1,19 @@
 import React, { useEffect, useState } from 'react'
+import styled from '@xstyled/styled-components'
+import { Box, Heading, ThemeProvider } from '@1e3/ui'
 
 import { getRunCount } from './utils'
+import { ReactComponent as Logo } from './logo.svg'
 
+const LogoWrapper = styled.div`
+  color: white;
+  width: 200px;
+
+  svg {
+    width: 100%;
+    fill: currentColor;
+  }
+`
 const App = () => {
   const [count, setCount] = useState(null)
 
@@ -9,9 +21,26 @@ const App = () => {
     getRunCount().then(setCount)
   }, [count])
 
-  if (!count) return '🏃'
-
-  return <div>I ran {count} days in a row</div>
+  return (
+    <ThemeProvider>
+      <Box
+        alignItems="center"
+        backgroundColor="primary.base"
+        display="flex"
+        justifyContent="center"
+        height="100vh"
+        width="100vw"
+      >
+        {count ? (
+          <Heading color="white">I ran {count} days in a row</Heading>
+        ) : (
+          <LogoWrapper>
+            <Logo />
+          </LogoWrapper>
+        )}
+      </Box>
+    </ThemeProvider>
+  )
 }
 
 export default App
