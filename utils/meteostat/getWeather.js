8a7727d8054af2ffede995fefd064340d1f2ea13
Rafael <rafaelfr@outlook.com>
2020-07-19 23:28:33 -0300

    fix: add return data verification to weather api

--- utils/meteostat/getWeather.js
+++ utils/meteostat/getWeather.js
@@ -5,6 +5,9 @@ import getHourlyData from './getHourlyData'
 
 export default async function getWeather(lat, lon, date) {
   const { data } = await getHourlyData(lat, lon, date)
+
+  if (!data) return null
+
   const { temp: temperature, coco } = data.find(({ time_local }) =>
     dayjs(date).minute(0).second(0).isSame(dayjs(time_local)),
   )
